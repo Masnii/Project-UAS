8302104be7d8957c7828d360fe01ef4a7a520363
Masnii <145676092+Masnii@users.noreply.github.com>
2023-12-23 17:03:27 +0800

    Add files via upload

---
+++ CodeFlappy/CodeFlappy/Lantai.java
@@ -0,0 +1,14 @@
+import greenfoot.*; 
+
+public class Lantai extends Actor
+{
+    public static int plg=0; 
+    public void act() 
+    {
+        int px=getX()-MyWorld.KECEPATAN;
+        if(px<-0.5*getImage().getWidth()){
+            px=plg;
+        }
+        setLocation(px,getY());
+    }    
+}
